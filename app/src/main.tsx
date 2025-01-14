cbf59298e7960fa19047db2039cddc79a3db3043
XtremeFire02 <e1121032@u.nus.edu>
2025-01-15 00:34:31 +0800

    Nicer UI and designs

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './App.css'
+import './index.css'
 import './styles/global.css'
+
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
